e8d395a49b3b1b984198aedf662337eba9deeb50
Artem Brilev <anbrilev@sberautotech.ru>
2023-04-24 22:34:12 +0300

    get acc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+
+const getAccount = () => 
+  window.ethereum.request({ method: 'eth_accounts' })
+  .catch((err) => {
+    console.error(err);
+  });
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+  useEffect(() => {
+    getAccount().then(accs => {
+      if (accs.length) {
+        setAccount(accs[0])
+      }
+    })
+  }, [])
   return (
     <div className="App">
+      {account ? <div>{account}</div> : <button>connect</button>}
     </div>
   );
 }
